e89862088da0da4a211dd585c04a3b051dd7afeb
hamdi2050 <hamdi.mar20@gmail.com>
2023-06-18 04:41:46 +0100

    Add normalize method test file

---
+++ test/normalize.test.ts
@@ -0,0 +1,19 @@
+import ArabicString from "../src/index";
+
+test("returns expected", () => {
+  const input = "بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ";
+  const expected = "بسم الله الرحمن الرحيم";
+  expect(ArabicString(input).normalize()).toBe(expected);
+});
+
+test("returns expected", () => {
+  const input = "بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ";
+  const expected = "بسم الله الرحمـٰن الرحيم";
+  expect(
+    ArabicString(input).normalize({
+      removeDiacritics: true,
+      removeSuperscriptAlef: false,
+      removeTatweel: false,
+    })
+  ).toBe(expected);
+});
